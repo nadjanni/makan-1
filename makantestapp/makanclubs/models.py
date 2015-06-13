e4ef62129f0a086bbc6c4b6304726bd370750a70
mattyhk <mattkerr@mit.edu>
2015-06-13 21:54:39 +0800

    Removed single club id from Eater, added Membership model

--- makantestapp/makanclubs/models.py
+++ makantestapp/makanclubs/models.py
@@ -1,19 +1,26 @@
 from django.db import models
 
+from django.utils import timezone
+
 # Create your models here.
 
 class Club(models.Model):
   club_name = models.CharField(max_length = 200)
-  create_date = models.DateTimeField('date created')
+  create_date = models.DateTimeField('date created', default=timezone.now())
+  def __unicode__(self):
+    return self.club_name
 
 class Eater(models.Model):
-  member_club = models.ForeignKey(Club)
   name = models.CharField(max_length = 200)
-  create_date = models.DateTimeField('date created')
+  create_date = models.DateTimeField('date created', default=timezone.now())
+  def __unicode__(self):
+    return self.name
 
 class Location(models.Model):
   location_name = models.CharField(max_length = 200)
-  create_date = models.DateTimeField('date created')
+  create_date = models.DateTimeField('date created', default=timezone.now())
+  def __unicode__(self):
+    return self.location_name
 
 class Experience(models.Model):
   reviewer = models.ForeignKey(Eater)
@@ -21,5 +28,13 @@ class Experience(models.Model):
   venue = models.ForeignKey(Location)
   appreciates = models.IntegerField(default = 0)
   experience_date = models.DateTimeField('date of experience')
-  create_date = models.DateTimeField('date created')
+  create_date = models.DateTimeField('date created', default=timezone.now())
+  def __unicode__(self):
+    return 'Experience of ' + str(self.reviewer) + ' at ' + str(self.venue)
+
+class Membership(models.Model):
+  eater = models.ForeignKey(Eater)
+  club = models.ForeignKey(Club)
+  def __unicode__(self):
+    return 'Membership of ' + str(self.eater), 'at', str(self.club)
 
